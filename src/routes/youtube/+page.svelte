111d4eafa817f159b0a64301d8279932714cc4c0
Script Raccoon <scriptraccoon@gmail.com>
2023-06-09 12:56:09 +0200

    improve youtube page

--- src/routes/youtube/+page.svelte
+++ src/routes/youtube/+page.svelte
@@ -16,23 +16,28 @@
 		YouTube channel
 	</a>
 	on web development. {#if data.stats}
-		Today, the channel has {data.stats.subscriberCount} subscribers and
-		{data.stats.videoCount} published videos.
+		Today, the channel has <strong
+			>{data.stats.subscriberCount}</strong
+		>
+		subscribers and
+		<strong>{data.stats.videoCount}</strong> published videos.
 	{/if}
 </p>
 
 <p>
-	On my channel I usually do not talk about one piece of technology
-	per video, what most other channels do. Instead, I focus on full
-	projects, which therefore cover several pieces of technology and
-	also put them more into context. These kinds of videos motivated and
-	helped me a lot when I started my journey in web development, which
-	is also why I like to produce these.
+	On my channel I usually do not talk about one concept per video,
+	what most other channels do. Instead, I focus on <strong
+		>full projects</strong
+	>, which therefore cover several concepts and pieces of technology
+	and also put them more into context. These kinds of videos motivated
+	and helped me a lot when I started my journey in web development,
+	which is also why I like to produce these.
 </p>
 <p>
-	I also choose a rather slow-paced approach, explaining every step,
-	so that also newbies should be able to follow. On the other hand, I
-	also use my YouTube channel to document my learnings. Both the
+	I also choose a rather slow-paced approach, explaining every <strong
+		>step by step</strong
+	>, so that also newbies should be able to follow. On the other hand,
+	I also use my YouTube channel to document my learnings. Both the
 	preparation and the recording of a video improve my understanding of
 	the subject. This is also why the videos started with rather basic
 	Vanilla JavaScript projects in 2019 and nowadays present full stack
@@ -40,17 +45,32 @@
 </p>
 
 <p>
-	I decided to do the channel in German (my native language), for the
-	sole reason that there are already lots of very good channels in
-	English, but only a few in German. Many people have asked me to
-	change to English or make subtitles. Sorry, but I will not do that.
-	There is a place for YouTube channels in every language. If you want
-	good English content, please check out the channels by Dave Gray,
-	Traversy Media, Kevin Powell, Web Dev Simplified, and The Net Ninja,
-	for example.
+	I decided to do the channel in <strong>German</strong> (my native
+	language), for the sole reason that there are already lots of very
+	good channels in English, but only a few in German. Many people have
+	asked me to change to English or make subtitles. Sorry, but I will
+	not do that. There is a place for YouTube channels in every
+	language. If you want good English content, please check out the
+	channels by
+	<a
+		href="https://www.youtube.com/@DaveGrayTeachesCode"
+		target="_blank">Dave Gray</a
+	>,
+	<a href="https://www.youtube.com/@TraversyMedia" target="_blank"
+		>Traversy Media</a
+	>,
+	<a href="https://www.youtube.com/@KevinPowell" target="_blank"
+		>Kevin Powell</a
+	>,
+	<a href="https://www.youtube.com/@WebDevSimplified" target="_blank"
+		>Web Dev Simplified</a
+	>, and
+	<a href="https://www.youtube.com/@NetNinja" target="_blank"
+		>The Net Ninja</a
+	>, for example.
 </p>
 <p>
-	You can always check out the code which I link in the video
-	description. Of course, the code has English variable names, and the
-	README also often includes some pieces of explanation.
+	You can always check out the <strong>code</strong> which I link in the
+	video description. Of course, the code has English variable names, and
+	the README also often includes some pieces of explanation.
 </p>
