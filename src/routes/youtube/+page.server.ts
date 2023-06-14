a37792b3993cadf08d238a87d6edbc565ad4955c
Script Raccoon <scriptraccoon@gmail.com>
2023-06-15 01:05:45 +0200

    use redis for caching

--- src/routes/youtube/+page.server.ts
+++ src/routes/youtube/+page.server.ts
@@ -1,18 +1,31 @@
 export const prerender = false;
 
-import { cached } from "$lib/server/cache";
+import { SECRET_REDIS_URL } from "$env/static/private";
+import Redis from "ioredis";
+const redis = new Redis(SECRET_REDIS_URL);
 
+import { redis_cached } from "$lib/server/redis-cache";
 import { get_youtube_stats } from "./stats";
 import { get_latest_video } from "./video";
 
 export const load = async () => {
-	return await get_youtube_data();
+	const stats = await get_cached_youtube_stats();
+	const video = await get_cached_latest_video();
+	return { stats, video };
 };
 
-const one_day = 24 * 60 * 60 * 1000;
+const one_day = 24 * 60 * 60;
 
-const get_youtube_data = cached(async () => {
-	const stats = await get_youtube_stats();
-	const video = await get_latest_video();
-	return { stats, video };
-}, one_day);
+const get_cached_youtube_stats = redis_cached(
+	redis,
+	"stats",
+	get_youtube_stats,
+	one_day,
+);
+
+const get_cached_latest_video = redis_cached(
+	redis,
+	"video",
+	get_latest_video,
+	one_day,
+);
