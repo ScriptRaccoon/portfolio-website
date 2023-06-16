0d93a748fab3d4ca2bd5cb8b9b5ea978334f3932
Script Raccoon <scriptraccoon@gmail.com>
2023-06-16 18:15:17 +0200

    initialize blog

---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import GoBack from "$lib/components/GoBack.svelte";
+
+	export let data;
+	const { title, updated, published } = data.attributes;
+	const { htmlContent } = data;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<GoBack />
+
+<h1>
+	{title}
+</h1>
+
+<div class="dates">
+	<div>
+		Published: {published.toLocaleDateString()}
+	</div>
+	{#if updated}
+		<div>
+			Last updasted: {updated.toLocaleDateString()}
+		</div>
+	{/if}
+</div>
+
+<main>
+	{@html htmlContent}
+</main>
+
+<style lang="scss">
+	h1 {
+		margin-block: 1rem;
+	}
+
+	.dates {
+		font-size: var(--small-font);
+		color: var(--gray-color);
+		display: flex;
+		flex-wrap: wrap;
+		column-gap: 0.5rem;
+		justify-content: space-between;
+	}
+
+	main {
+		:global(h2) {
+			margin-top: 1.5rem;
+		}
+
+		:global(pre) {
+			border-radius: 0.4rem;
+			font-size: var(--small-font);
+			padding: 1rem;
+			overflow: scroll;
+			background-color: #333;
+			color: white;
+			max-height: 30rem;
+		}
+
+		:global(blockquote) {
+			margin-left: 1.5rem;
+			padding-left: 0.5rem;
+			color: var(--gray-color);
+			font-size: var(--small-font);
+		}
+	}
+</style>
---
+++ src/routes/blog/types.ts
@@ -0,0 +1,7 @@
+export type frontmatter = {
+	id: string;
+	title: string;
+	published: Date;
+	updated: Date | null;
+	public: boolean;
+};
