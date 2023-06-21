df7c8d3b964bdc8ec0c0fd4bd933fc10ce89c3e6
Script Raccoon <scriptraccoon@gmail.com>
2023-06-21 20:09:26 +0200

    save theme in localStorage

--- src/lib/components/ThemeToggler.svelte
+++ src/lib/components/ThemeToggler.svelte
@@ -8,21 +8,48 @@
 
 	let theme: theme_type = "light";
 
-	if (
-		browser &&
-		window.matchMedia &&
-		window.matchMedia("(prefers-color-scheme: dark)").matches
-	) {
-		theme = "dark";
-		document.documentElement.classList.add("dark");
+	if (browser) compute_user_preference();
+
+	function compute_user_preference() {
+		const saved_theme = localStorage.getItem(
+			"theme",
+		) as theme_type | null;
+
+		if (saved_theme) {
+			theme = saved_theme;
+			if (theme === "dark") {
+				document.documentElement.classList.add("dark");
+			}
+		} else if (
+			window.matchMedia &&
+			window.matchMedia("(prefers-color-scheme: dark)").matches
+		) {
+			theme = "dark";
+			document.documentElement.classList.add("dark");
+		}
 	}
 
 	function toggle_theme() {
 		theme = theme === "light" ? "dark" : "light";
 		document.documentElement.classList.toggle("dark");
+		localStorage.setItem("theme", theme);
 	}
 </script>
 
-<button on:click={toggle_theme} aria-label="Toggle dark mode">
+<button
+	on:click={toggle_theme}
+	aria-label="Toggle theme, current theme is {theme}"
+>
 	<Fa icon={theme === "dark" ? faMoon : faSun} />
 </button>
+
+<style>
+	button {
+		width: 1.5rem;
+		height: 1.5rem;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
