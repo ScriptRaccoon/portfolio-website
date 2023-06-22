03f50ec614f3c458cd235e916de70be55af567be
Script Raccoon <scriptraccoon@gmail.com>
2023-06-23 01:02:41 +0200

    use cookies for persistent theme

--- src/lib/components/ThemeToggler.svelte
+++ src/lib/components/ThemeToggler.svelte
@@ -1,46 +1,49 @@
 <script lang="ts">
-	import { browser } from "$app/environment";
 	import Fa from "svelte-fa";
 	import { faMoon } from "@fortawesome/free-solid-svg-icons";
 	import { faSun } from "@fortawesome/free-regular-svg-icons";
+	import { onMount } from "svelte";
+	import { THEMES } from "$lib/shared/config";
 
-	type theme_type = "light" | "dark";
+	let current_theme: string;
 
-	let theme: theme_type = "light";
-
-	if (browser) compute_user_preference();
-
-	function compute_user_preference() {
-		const saved_theme = localStorage.getItem(
-			"theme",
-		) as theme_type | null;
-
-		if (saved_theme) {
-			theme = saved_theme;
-			if (theme === "dark") {
-				document.documentElement.classList.add("dark");
-			}
-		} else if (
-			window.matchMedia &&
-			window.matchMedia("(prefers-color-scheme: dark)").matches
-		) {
-			theme = "dark";
-			document.documentElement.classList.add("dark");
+	onMount(() => {
+		const saved_theme =
+			document.documentElement.getAttribute("data-theme");
+		if (saved_theme && Object.values(THEMES).includes(saved_theme)) {
+			current_theme = saved_theme;
+			return;
 		}
+
+		const preference_is_dark = window.matchMedia(
+			"(prefers-color-scheme: dark)",
+		).matches;
+
+		const theme = preference_is_dark ? THEMES.DARK : THEMES.LIGHT;
+
+		set_theme(theme);
+	});
+
+	function set_theme(theme: string) {
+		if (!Object.values(THEMES).includes(theme)) return;
+		const one_year = 60 * 60 * 24 * 365;
+		document.cookie = `theme=${theme}; max-age=${one_year}; path=/`;
+		document.documentElement.setAttribute("data-theme", theme);
+		current_theme = theme;
 	}
 
-	function toggle_theme() {
-		theme = theme === "light" ? "dark" : "light";
-		document.documentElement.classList.toggle("dark");
-		localStorage.setItem("theme", theme);
+	function toggle_theme(): void {
+		const theme =
+			current_theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+		set_theme(theme);
 	}
 </script>
 
 <button
 	on:click={toggle_theme}
-	aria-label="Toggle theme, current theme is {theme}"
+	aria-label="Toggle theme, current theme is {current_theme}"
 >
-	<Fa icon={theme === "dark" ? faMoon : faSun} />
+	<Fa icon={current_theme === THEMES.DARK ? faMoon : faSun} />
 </button>
 
 <style>
