92b7b49d62763f230311f2861cd8de237504d926
Script Raccoon <scriptraccoon@gmail.com>
2023-07-10 18:27:32 +0200

    adm page

---
+++ src/routes/adm/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+	import { onDestroy, onMount } from "svelte";
+	let show = false;
+
+	function key_handler(e: KeyboardEvent) {
+		if (e.key === "a") {
+			show = true;
+			window.removeEventListener("keydown", key_handler);
+		}
+	}
+
+	onMount(() => {
+		if (browser) window.addEventListener("keydown", key_handler);
+	});
+
+	onDestroy(() => {
+		if (browser) window.removeEventListener("keydown", key_handler);
+	});
+</script>
+
+<h1>Admin</h1>
+
+{#if show}
+	<ul>
+		<li>
+			<a
+				href="https://github.com/ScriptRaccoon/portfolio-website"
+				target="_blank">Repository</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://console.upstash.com/redis/cf05e0a5-9ce9-4174-9c71-2956f3edee90"
+				target="_blank">Redis</a
+			>
+		</li>
+
+		<li>
+			<a href="https://scriptraccoon.dev" target="_blank">Website</a>
+		</li>
+		<li>
+			<a
+				href="https://www.your-analytics.org/scriptraccoon.dev"
+				target="_blank">Your-Analytics</a
+			>
+		</li>
+		<li>
+			<a
+				href="https://app.netlify.com/sites/scriptraccoon/deploys"
+				target="_blank">Deployment</a
+			>
+		</li>
+	</ul>
+{:else}
+	Nothing to see here. Go back <a href="/">Home</a>.
+{/if}
