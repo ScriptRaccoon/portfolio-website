aa252e8886d029ce14bbd17976cf4eb9ade53769
Script Raccoon <scriptraccoon@gmail.com>
2023-07-30 00:30:15 +0200

    update social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import logo from "$lib/assets/ScriptRaccoon.png";
+	import {
+		faCodepen,
+		faGit,
+		faGithub,
+		faTwitter,
+	} from "@fortawesome/free-brands-svg-icons";
+	import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
 </script>
 
 <h1>Script Raccoon</h1>
@@ -16,23 +24,44 @@
 	<img src={logo} alt="raccoon logo" />
 </p>
 
-<p>
-	The best way to contact me is via email: <a
-		href="mai&#108;t&#111;&#58;s&#99;r%69pt%72%61cc%6F&#111;%6E%40&#103;mail&#46;c%6&#70;m"
-		>sc&#114;i&#112;tr&#97;ccoo&#110;&#64;g&#109;&#97;il&#46;com</a
-	>
-</p>
+<ul class="no-bullets socials">
+	<li>
+		<a
+			aria-label="Email"
+			href="mai&#108;t&#111;&#58;s&#99;r%69pt%72%61cc%6F&#111;%6E%40&#103;mail&#46;c%6&#70;m"
+		>
+			<Fa icon={faEnvelope} />
+		</a>
+	</li>
+	<li>
+		<a
+			aria-label="Twitter"
+			href="https://twitter.com/scriptraccoon"
+			target="_blank"
+		>
+			<Fa icon={faTwitter} /></a
+		>
+	</li>
 
-<p>
-	You can also find me on
-	<a href="https://twitter.com/scriptraccoon" target="_blank">
-		Twitter</a
-	>
-	and
-	<a href="https://github.com/scriptraccoon" target="_blank">
-		GitHub</a
-	>.
-</p>
+	<li>
+		<a
+			aria-label="GitHub"
+			href="https://github.com/scriptraccoon"
+			target="_blank"
+		>
+			<Fa icon={faGithub} />
+		</a>
+	</li>
+	<li>
+		<a
+			aria-label="Codepen"
+			href="https://codepen.com/scriptraccoon"
+			target="_blank"
+		>
+			<Fa icon={faCodepen} />
+		</a>
+	</li>
+</ul>
 
 <style lang="scss">
 	.logo-container {
@@ -44,5 +73,12 @@
 			border-radius: 50%;
 			box-shadow: 0rem 0rem 1rem var(--shadow-color);
 		}
+	}
+	.socials {
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		gap: 1.5rem;
+		font-size: 1.5rem;
 	}
 </style>
