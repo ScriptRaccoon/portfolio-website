fc8745614cd4bb486e76dd7cc69f96a43650746b
Script Raccoon <scriptraccoon@gmail.com>
2024-01-13 10:09:24 +0100

    overhaul of Nav, add animation

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from "$app/stores";
+	import { getContext } from "svelte";
 
 	import logo from "$lib/assets/ScriptRaccoon.png";
 	import ThemeToggler from "./ThemeToggler.svelte";
@@ -18,28 +19,62 @@
 
 	type link = {
 		name: string;
-		href: string;
-		icon: IconDefinitionSolid | IconDefinitionBrand;
+		path: string;
+		icon: IconDefinitionSolid | IconDefinitionBrand | null;
+		nested: boolean;
 	};
 
 	const links: link[] = [
-		{ name: "YouTube", href: "/youtube", icon: faYoutube },
-		{ name: "Projects", href: "/projects", icon: faClipboardList },
-		{ name: "Blog", href: "/blog", icon: faPenToSquare },
-		{ name: "About", href: "/about", icon: faCircleQuestion },
+		{ name: "Home", path: "/", icon: null, nested: false },
+		{
+			name: "YouTube",
+			path: "/youtube",
+			icon: faYoutube,
+			nested: false,
+		},
+		{
+			name: "Projects",
+			path: "/projects",
+			icon: faClipboardList,
+			nested: true,
+		},
+		{
+			name: "Blog",
+			path: "/blog",
+			icon: faPenToSquare,
+			nested: true,
+		},
+		{
+			name: "About",
+			path: "/about",
+			icon: faCircleQuestion,
+			nested: false,
+		},
 	];
+
+	$: current_path = $page.url.pathname;
+
+	$: page_pos = links.findIndex(
+		(link) =>
+			link.path === current_path ||
+			(link.nested && current_path.startsWith(link.path)),
+	);
+
+	const items_count = links.length + 1;
+
+	const allow_animation = getContext("allow_animation");
 </script>
 
 <nav>
-	<ul class="no-bullets">
-		<li class:current={$page.url.pathname === "/"}>
+	<ul class="no-bullets" style:--items-count={items_count}>
+		<li class:current={current_path === "/"}>
 			<a href="/">
 				<img src={logo} alt="Home" class="logo" />
 			</a>
 		</li>
-		{#each links as { name, href, icon }}
-			<li class:current={$page.url.pathname.startsWith(href)}>
-				<a {href}>
+		{#each links.slice(1) as { name, path, icon }}
+			<li class:current={current_path.startsWith(path)}>
+				<a href={path}>
 					<Fa {icon} />
 					<span class="name">{name}</span>
 				</a>
@@ -48,25 +83,31 @@
 		<li>
 			<ThemeToggler />
 		</li>
+		<div
+			aria-hidden="true"
+			class="underline"
+			style:--pos={page_pos}
+			class:invisible={page_pos < 0}
+			class:animated={allow_animation}
+		/>
 	</ul>
 </nav>
 
 <style>
 	nav {
-		padding-block: 1rem;
+		padding-block: 0.5rem;
 		border-bottom: 1px solid var(--border-color);
 		position: sticky;
 		top: 0;
 		background-color: var(--bg-color);
 		box-shadow: 0rem -2rem 0rem 2rem var(--bg-color);
-		z-index: 1;
+		z-index: 5;
 	}
 
 	ul {
-		display: flex;
-		justify-content: space-between;
-		flex-wrap: wrap;
-		gap: 0.75rem;
+		display: grid;
+		grid-template-columns: repeat(var(--items-count), 1fr);
+		position: relative;
 	}
 
 	.logo {
@@ -80,36 +121,52 @@
 	}
 
 	li {
-		position: relative;
 		display: flex;
 		align-items: center;
+		justify-content: center;
 		padding-block: 0.1rem;
-	}
-
-	li.current::after {
-		content: "";
-		position: absolute;
-		left: 0;
-		right: 0;
-		bottom: -0.15rem;
-		height: 0.1rem;
-		background-color: var(--accent-color);
-		border-radius: 100vw;
 	}
 
 	.name {
 		font-size: var(--small-font);
+		position: absolute;
+		left: -100vw;
 	}
 
-	@media (max-width: 38rem) {
+	.underline {
+		position: absolute;
+		height: 0.1rem;
+		top: 100%;
+		left: 0;
+		transform: translateX(calc(var(--pos) * 100%));
+		width: calc(100% / var(--items-count));
+	}
+
+	.underline.animated {
+		transition: transform 200ms ease, opacity 200ms ease;
+	}
+
+	.underline.invisible {
+		opacity: 0;
+	}
+
+	.underline::before {
+		content: "";
+		position: absolute;
+		left: 10%;
+		width: 80%;
+		height: 100%;
+		background-color: var(--accent-color);
+	}
+
+	@media (min-width: 38rem) {
 		nav {
-			padding-block: 0.5rem;
+			padding-block: 1rem;
 		}
 
-		li:not(.current) .name {
-			/* visually hidden */
-			position: absolute;
-			left: -100000px;
+		.name {
+			position: unset;
+			left: unset;
 		}
 	}
 </style>
