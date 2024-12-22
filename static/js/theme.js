aceefffb904e6c8103f28bde00c71a824ddb19d2
Script Raccoon <scriptraccoon@gmail.com>
2024-12-22 10:43:58 +0100

    add validation to theme script

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,12 +1,14 @@
-const saved_theme = localStorage.getItem("theme");
+if (typeof window !== undefined) {
+	const saved_theme = localStorage.getItem("theme");
 
-if (saved_theme) {
-	document.body.setAttribute("data-theme", saved_theme);
-} else {
-	const prefers_dark = window.matchMedia(
-		"(prefers-color-scheme: dark)",
-	).matches;
-	const theme = prefers_dark ? "dark" : "light";
-	document.body.setAttribute("data-theme", theme);
-	localStorage.setItem("theme", theme);
+	if (saved_theme === "light" || saved_theme === "dark") {
+		document.body.setAttribute("data-theme", saved_theme);
+	} else {
+		const prefers_dark = window.matchMedia(
+			"(prefers-color-scheme: dark)",
+		).matches;
+		const theme = prefers_dark ? "dark" : "light";
+		document.body.setAttribute("data-theme", theme);
+		localStorage.setItem("theme", theme);
+	}
 }
