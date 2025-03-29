5314454776cedfa91042b985a3546b349051a864
Script Raccoon <scriptraccoon@gmail.com>
2025-03-29 12:57:26 +0100

    add caret to expand component

--- src/lib/components/Expand.svelte
+++ src/lib/components/Expand.svelte
@@ -2,6 +2,11 @@
 	import { getContext } from 'svelte'
 	import { slide } from 'svelte/transition'
 	import type { Snippet } from 'svelte'
+	import Fa from 'svelte-fa'
+	import {
+		faCaretDown,
+		faCaretRight,
+	} from '@fortawesome/free-solid-svg-icons'
 
 	type Props = {
 		summary: string
@@ -28,7 +33,8 @@
 	onclick={toggle_open}
 	class:closed={!open}
 >
-	{summary}
+	<span>{summary}</span>
+	<Fa icon={open ? faCaretRight : faCaretDown} />
 </button>
 
 <div id={content_id} aria-labelledby={button_id}>
@@ -40,11 +46,7 @@
 </div>
 
 <style>
-	button {
-		transition: color 200ms linear;
-	}
-
-	button.closed {
-		color: var(--secondary-font-color);
+	span {
+		margin-right: 0.25rem;
 	}
 </style>
