aa06c3af17f30c64fcc445e396c2b07f72f7a939
Script Raccoon <scriptraccoon@gmail.com>
2026-02-06 22:04:54 +0100

    custom error page with meta tags

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/state'
+	import GoBack from '$lib/components/GoBack.svelte'
+	import MetaTags from '$lib/components/MetaTags.svelte'
+</script>
+
+<MetaTags title="Script Raccoon - Error" description="Error Page" />
+
+<h1>
+	{page.status}
+</h1>
+
+<p>
+	{page.error?.message}
+</p>
+
+<GoBack />
