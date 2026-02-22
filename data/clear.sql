4afa33f395b9407b0fa9684ccbdd8768672ae04e
Script Raccoon <scriptraccoon@gmail.com>
2026-02-22 11:55:52 +0100

    clear sql

---
+++ data/clear.sql
@@ -0,0 +1,19 @@
+DELETE FROM sessions_monthly;
+
+DELETE FROM visits_monthly;
+
+DELETE FROM referrers_total;
+
+DELETE FROM browsers_total;
+
+DELETE FROM os_total;
+
+DELETE FROM countries_total;
+
+DELETE FROM themes_total;
+
+DELETE FROM device_types_total;
+
+DELETE FROM visits_live;
+
+DELETE FROM sessions_live;
